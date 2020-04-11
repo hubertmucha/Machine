5240576af196c740dbd97ec70bac49fb49759d65
Hubert Mucha <58261346+hubertmucha@users.noreply.github.com>
2020-04-11 13:10:41 +0200

    kernel svm

--- Kernel SVM/main.py
+++ Kernel SVM/main.py
@@ -18,7 +18,7 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 from sklearn.svm import SVC
-classifier = SVC(kernel='linear')
+classifier = SVC(kernel='rbf')
 classifier.fit(X_train,y_train)
 
 y_pred = classifier.predict(X_test)
@@ -28,7 +28,7 @@ from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
 from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
+X_set, y_set = X_test, y_test
 #X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
@@ -39,6 +39,3 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.show()
-
-
